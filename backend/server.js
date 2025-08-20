7fe2b559495dcce4b0e137469a6165ecb03b6643
2tidalslightly <213461800+2tidalslightly@users.noreply.github.com>
2025-08-19 19:14:41 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@
 //IMPORTANT: Follow .env configuration instructions in utilities/jwt.js and db.js files
 
 const express = require("express");                                                                         //Import Express for backend
+const cors = require("cors");                                                                               //Import Cors for frontend backend communication
 const dotenv = require("dotenv");                                                                           //Import Dotenv for environment variables to keep passwords and keys separate from source code
 dotenv.config();
 
 const app = express();                                                                                      //Establish server
 app.use(express.json());                                                                                    //Parse incoming requests
+app.use(cors());                                                                                            //Frontend backend communication
 app.use("/recommendation", require("./routes/recommendation"));                                             //Configure path to recommendation route definitions
 app.use("/user", require("./routes/user"));                                                                 //Configure path to user route definitions
 app.use("/watchlist", require("./routes/watchlist"));                                                       //Configure path to watchlist route definitions
